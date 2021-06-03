14df452d78e755f30efd2d013b09dfe81376a675
Ben Meyer <bmeyer@Bens-Mac-mini.local>
2021-06-02 23:44:09 -0400

    Added vee-validate to validate city and state input fields

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -3,11 +3,21 @@
     <h2>Welcome to the temperature forecast</h2>
 
     <h4>Please enter your city and state</h4>
-    <input v-model="city" placeholder="City">
-    <input v-model="state" placeholder="State">
-    <br />
-    <br />
-    <button v-on:click="getForecast">Get forecast</button>
+    <ValidationObserver v-slot="{ invalid }">
+      <form @submit.prevent="getForecast">
+        <ValidationProvider name="City" rules="required" v-slot="{ errors }">
+          <input v-model="city" type="text">
+          <span>{{ errors[0] }}</span>
+        </ValidationProvider>
+
+        <ValidationProvider name="State" rules="required" v-slot="{ errors }">
+          <input v-model="state" type="text">
+          <span>{{ errors[0] }}</span>
+        </ValidationProvider>
+
+        <button type="submit" :disabled="invalid">Get forecast</button>
+      </form>
+    </ValidationObserver>
 
     <p>
       Cache hit: {{ cacheHit }}
@@ -24,6 +34,12 @@
 </template>
 
 <script>
+import { ValidationObserver, ValidationProvider } from 'vee-validate';
+import { extend } from 'vee-validate';
+import { required } from 'vee-validate/dist/rules';
+
+extend('required', required);
+
 export default {
   data() {
     return {
@@ -48,6 +64,10 @@ export default {
         this.forecast = forecastJson.forecast;
       }
     }
+  },
+  components: {
+    ValidationObserver,
+    ValidationProvider
   }
 }
 </script>
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/no-babel'
+  preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
+  transform: {
+    'vee-validate/dist/rules': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!vee-validate/dist/rules)',
+  ],
 }
